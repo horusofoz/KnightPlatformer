e1402a6f45c8c5903c6e0983b527901f88ff2c12
Jeff <horusofoz@gmail.com>
2018-03-28 15:33:52 +1030

    Tutorial 01 complete.

---
+++ KnightPlatformer/KnightPlatformer/Player.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KnightPlatformer
+{
+    class Player
+    {
+        Sprite sprite = new Sprite();
+
+        public Player()
+        {
+        }
+
+        public void Load(ContentManager content)
+        {
+            sprite.Load(content, "hero");
+        }
+
+        public void Update(float deltaTime)
+        {
+            sprite.Update(deltaTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
